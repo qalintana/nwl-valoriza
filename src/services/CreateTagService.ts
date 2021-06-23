2b95b722acf00ee04f9df968632843c1e959640f
qalintana <alintana18@gmail.com>
2021-06-23 23:29:55 +0100

    finalizando a aula 3

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from 'typeorm';
+import { TagsRepository } from '../repositories/TagsRepository';
+
+interface ITagRequest {
+  name: string;
+}
+
+class CreateTagService {
+  async execute(name: string) {
+    const tagsRepositories = getCustomRepository(TagsRepository);
+
+    if (!name) {
+      throw new Error('incorrect name');
+    }
+
+    const tagAlreadExists = await tagsRepositories.findOne({ name });
+
+    if (tagAlreadExists) {
+      throw new Error('Tag Already exists!');
+    }
+
+    const tag = tagsRepositories.create({ name });
+
+    await tagsRepositories.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
