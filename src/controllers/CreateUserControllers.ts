32958a3e8d7c537f03afcc25c1f9cf0049150b47
qalintana <alintana18@gmail.com>
2021-06-24 07:19:55 +0100

    adicionando criptografia nas passwords

--- src/controllers/CreateUserControllers.ts
+++ src/controllers/CreateUserControllers.ts
@@ -3,11 +3,16 @@ import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUsersControllers {
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
+    const { name, email, admin, password } = request.body;
 
     const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, admin, email });
+    const user = await createUserService.execute({
+      name,
+      admin,
+      email,
+      password,
+    });
 
     return response.json(user);
   }
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,17 @@
 import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import { UsersRepositories } from '../repositories/UserRepository';
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepositories = getCustomRepository(UsersRepositories);
 
     if (!email) {
@@ -21,7 +24,14 @@ class CreateUserService {
       throw new Error('User already exists');
     }
 
-    const user = usersRepositories.create({ name, email, admin });
+    const passwordHash = await hash(password, 8);
+
+    const user = usersRepositories.create({
+      name,
+      email,
+      admin,
+      password: passwordHash,
+    });
 
     await usersRepositories.save(user);
 
